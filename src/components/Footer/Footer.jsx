f3ef4a45e235ce56e2cc8a8ab208cede42ef2f93
biplap12 <biplap.neupane2020@gmail.com>
2022-08-28 14:47:12 +0545

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Footer.css";
+import Wave from "../../img/wave.png";
+import Insta from "@iconscout/react-unicons/icons/uil-instagram";
+import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
+import Gitub from "@iconscout/react-unicons/icons/uil-github";
+
+const Footer = () => {
+	return (
+		<div className="footer">
+			<img src={Wave} alt="" style={{ width: "100%" }} />
+			<div className="f-content">
+				<span>biplap.neupane2020@gmail.com</span>
+				<div className="f-icons">
+					<a href="https://www.instagram.com/">
+						<Insta color="white" size={"3rem"} />
+					</a>
+					<a href="https://www.facebook.com/login/">
+						{" "}
+						<Facebook color="white" size={"3rem"} />
+					</a>
+					<a href="https://github.com/biplap12">
+						{" "}
+						<Gitub color="white" size={"3rem"} />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
