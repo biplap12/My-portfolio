e47d95bf89686d7c194de2ed95a3bea72f6a57c1
Biplap Neupane <biplap.neupane2020@gmail.com>
2025-07-28 22:16:27 +0545

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,8 +6,63 @@ import nanofinance from "../img/nanofinance.png";
 import BnEstate from "../img/BnEstate.png";
 import imageSearchApp from "../img/imageSearchApp.png";
 import shreeDharapaniSamaj from "../img/shreedharapani.png";
-
+import highfiveadventure from "../img/highfiveadventure.png";
+import infinityAdventureNepal from "../img/infinityAdventureNepal.png";
+import epeakExpedition from "../img/epeak-expedition.png";
+import kingfisherRealEstate from "../img/kingfisherRealEstate.png";
+import flyeastnepal from "../img/flyeast.png";
 const projects = [
+  {
+    slug:"shree-dharapani-samaj",
+    title: "Shree Dharapani Samaj",
+    image: shreeDharapaniSamaj,
+    alt: "Shree Dharapani Samaj Project made with Next.js and Tailwind CSS by Biplap Neupane",
+    url: "https://dharapani-neupane-samaj.biplapneupane.com.np/",
+    description: "Shree Dharapani Neupane Samaj is a nonprofit organization dedicated to fostering unity, preserving culture, and supporting the well-being of the Neupane community from Dharapani. This website, designed and developed as a solo project by me using Next.js and Tailwind CSS, serves as an informative and user-friendly platform for community members to stay updated on events, notices, social initiatives, and organizational news. It aims to strengthen connections within the community by providing easy access to important resources and opportunities to engage and contribute."
+
+  },
+  {
+    slug: "highfiveadventure",
+    title: "HighFiveAdventure",
+    image: highfiveadventure, 
+    alt: "HighFiveAdventure Project made with Next.js and Tailwind CSS by Biplap Neupane",
+    url: "https://highfiveadventure.com/",
+    description: "HighFiveAdventure is a professionally developed website created as part of a team project in a corporate environment. Designed for a trekking and adventure travel company, the platform highlights various tours, itineraries, and booking options, allowing users to explore the natural beauty of Nepal through guided experiences. Built using Next.js and Tailwind CSS, the site delivers a fast, responsive, and user-friendly experience tailored to both local and international adventure seekers."
+
+  },{
+    slug:"flyeastnepal",
+    title: "FlyEastNepal",
+    image: flyeastnepal,
+    alt: "FlyEastNepal Project made with Next.js and Tailwind CSS by Biplap Neupane",
+    url: "https://flyeastnepal.com/",
+    description: "FlyEastNepal is a professionally developed website created as part of a team project in a corporate environment. Designed for a trekking and adventure travel company, the platform highlights various tours, itineraries, and booking options, allowing users to explore the natural beauty of Nepal through guided experiences. Built using Next.js and Tailwind CSS, the site delivers a fast, responsive, and user-friendly experience tailored to both local and international adventure seekers."
+  },
+  {
+    slug:"infinity-adventure-nepal",
+    title: "Infinity Adventure Nepal",
+    image: infinityAdventureNepal,
+    alt: "Infinity Adventure Nepal Project made with Next.js and Tailwind CSS by Biplap Neupane",
+    url: "https://infinityadventurenepal.com/",
+    description: "Infinity Adventure Nepal is a modern, team-built web platform developed in a professional setting for a trekking and adventure travel company. It features comprehensive tour details, itineraries, and booking capabilities, enabling users to plan and experience guided adventures across Nepal. Developed using Next.js and Tailwind CSS, the site delivers a fast, responsive, and engaging user interface tailored to adventure seekers worldwide."
+
+  },
+  {
+    slug:"epeak-expedition",
+    title:"Epeak Expedition",
+    image:epeakExpedition,
+    alt:"Epeak Expedition Project made with Next.js and Tailwind CSS by Biplap Neupane",
+    url:"https://epeakexpedition.com/home",
+    description: "Epeak Expedition is a professionally developed website created for a trekking and adventure travel company. Designed with Next.js and Tailwind CSS, the platform highlights a variety of tour packages, detailed itineraries, and booking options to help travelers experience the breathtaking landscapes of Nepal through expertly guided adventures."
+
+  },
+  {
+    slug:"kingfisher-real-estate",
+    title: "Kingfisher Real Estate",
+    image: kingfisherRealEstate,
+    alt: "Kingfisher Real Estate Project made with React.js and Tailwind CSS by Biplap Neupane",
+    url: "https://kingfisherrealestate-live.vercel.app/",
+    description: "Kingfisher Real Estate is a fully functional real estate web application developed as a solo project by me for a client based in Dubai. This professional-grade platform includes a custom-designed frontend, backend, and admin dashboard to manage property listings and user data efficiently. Built using React.js and Tailwind CSS, the application delivers a clean, responsive, and user-friendly interface for both property managers and clients, ensuring smooth navigation and streamlined property management."
+  },
   {
     slug:"bn-store",
     title: "BN Store",
@@ -17,18 +72,10 @@ const projects = [
     description: "BN Store is an e-commerce platform that allows users to purchase groceries and other household items online. The platform is built using React.js and Tailwind CSS, and is designed to be user-friendly and easy to navigate.",
   },
   {
-    slug:"shree-dharapani-samaj",
-    title: "Shree Dharapani Samaj",
-    image: shreeDharapaniSamaj,
-    alt: "Shree Dharapani Samaj Project made with React.js and Tailwind CSS by Biplap Neupane",
-    url: "https://dharapani-neupane-samaj.biplapneupane.com.np/",
-    description: "Shree Dharapani Samaj is a website that provides information about the Shree Dharapani Samaj. The website is built using React.js and Tailwind CSS, and is designed to be user-friendly and easy to navigate.",
-  },
-  {
     slug:"employee-management-system",
     title: "Employee Management System",
     image: epm,
-    alt: "Employee Management System Project made with React.js and Tailwind CSS by Biplap Neupane",
+    alt: "Employee Management System Project made with Python Django and Bootstrap html and CSS by Biplap Neupane",
     url: "https://github.com/biplap12/Attendance-Management-using-Face-Recognition.git",
     description: "Employee Management System is a web application that allows users to manage their employees and their attendance. The application is built using React.js and Tailwind CSS, and is designed to be user-friendly and easy to navigate.",
   },
@@ -64,22 +111,7 @@ const projects = [
     url: "https://bn-estate.vercel.app/",
     description: "BnEstate is a web application that allows users to manage their BnEstate and their patients. The application is built using React.js and Tailwind CSS, and is designed to be user-friendly and easy to navigate.",
   },
-  {
-    slug:"image-search-app",
-    title: "Image Search App",
-    image: imageSearchApp,
-    alt: "Images Search App Project made with React.js and Tailwind CSS by Biplap Neupane",
-    url: "https://github.com/biplap12/Image_Search_App.git",
-    description: "Image Search App is a web application that allows users to search for images and their patients. The application is built using React.js and Tailwind CSS, and is designed to be user-friendly and easy to navigate.",
-  },
-  {
-    slug:"attendance-management-system",
-    title: "Attendance Management System",
-    image: epm,
-    alt: "Attendance Management System Project made with React.js and Tailwind CSS by Biplap Neupane",
-    url: "https://github.com/biplap12/Attendance-Management-using-Face-Recognition.git",
-    description: "Attendance Management System is a web application that allows users to manage their attendance and their patients. The application is built using React.js and Tailwind CSS, and is designed to be user-friendly and easy to navigate."
-  }
+  
 ];
 
 export default projects;
